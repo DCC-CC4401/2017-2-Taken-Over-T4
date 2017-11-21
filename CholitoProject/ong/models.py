90c41f7199093d54eabd6f66f989607cbcf479bd
valiryan <brauliotorreshenriquez@gmail.com>
2017-11-20 21:37:56 -0300

    save

--- CholitoProject/ong/models.py
+++ CholitoProject/ong/models.py
@@ -1,7 +1,10 @@
+
 from django.db import models
 
 # Create your models here.
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, Permission
+
+from django.shortcuts import redirect
 
 # Create your models here.
 
@@ -10,8 +13,25 @@ class ONG(models.Model):
     name = models.TextField(max_length=200)
     lat = models.DecimalField(max_digits=9, decimal_places=6)
     lng = models.DecimalField(max_digits=9, decimal_places=6)
-    directions = models.TextField(max_length=200, null=True)
+
+    def __str__(self):
+        return self.name
 
 
 class ONGUser(User):
+    user = models.OneToOneField(User)
     ong = models.ForeignKey('ONG')
+
+    def __str__(self):
+        return self.ong.name
+
+    def get_index(self, request, context):
+        return redirect('/ong/')
+
+    def save(self, *args, **kwargs):
+        super(ONGUser, self).save(*args, **kwargs)
+        if not self.user.has_perm('ong_user_access'):
+            permission = Permission.objects.get(
+                codename='ong_user_access')
+            self.user.user_permissions.add(permission)
+        self.user.save()
